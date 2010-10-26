62ead79b73f4dc3f0d68df19cf9c2b61bc5cb9a2
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2010-10-26 21:56:48 +0000

    Fixed link layer errors

--- src/stage/networking/layers/link/ethernet.py
+++ src/stage/networking/layers/link/ethernet.py
@@ -1,3 +1,7 @@
+from construct import *
+from stage.networking.layers.link.linklayer import LinkLayer
+from stage.utils.packetbuilder import PacketBuilder
+
 class EthernetLinkLayer(LinkLayer) :
     def __init__(self) :
         LinkLayer.__init__(self)
@@ -12,16 +16,16 @@ class EthernetLinkLayer(LinkLayer) :
 
     def send(self, internet_proto, dest_ip, data) :
         header = PacketBuilder.build(self._header_struct, 
-            source_mac = self._interface.get_mac()
-            destination_mac = self._ip_to_mac(dest_ip)
+            source_mac = self._interface.get_mac(),
+            destination_mac = self._ip_to_mac(dest_ip),
             internet_proto = IPv4)
         trailer = PacketBuilder.build(self._trailer_struct,
-            crc32 = 0)
+        crc32 = 0)
         full_frame = header + data + trailer
         self._interface.send(full_frame)
 
     def recv(self, data) :
-        header = PacketBuilder.parse(data[0:self._header_struct.sizeof())
+        header = PacketBuilder.parse(data[0:self._header_struct.sizeof()])
 
         data = data[self._header_struct.sizeof():]
         data = data[:-self._trailer_struct.sizeof()]
