feeb97eab48cc2b3bf7e05d5b3122598e245eecf
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2011-01-07 12:05:52 +0000

    Daemons now terminate local entities properly

--- stage/startupdaemon.py
+++ stage/startupdaemon.py
@@ -1,3 +1,4 @@
+import signal
 import sys
 import subprocess
 from stage.eventapi import EventAPI
@@ -7,19 +8,23 @@ class StartupDaemon :
     def __init__(self, phys_id) :
         self._event_api = EventAPI()
         self._phys_id = phys_id
+        self._running_pids = set([])
 
     def _on_startup(self, event) :
-        self._terminate_all()
+        self.terminate_all()
         self._event_api.unsubscribe_all(StartupEvent)
         self._event_api.subscribe(StartSimulationEvent, self._on_sim_start)
         self._event_api.publish(AckStartupEvent(self._phys_id))
 
-    def _terminate_all(self) :
-        #TODO: Kill all existing node processes
-        pass
+    def terminate_all(self) :
+        for p in self._running_pids :
+            p.kill()
+        self._running_pids.clear()
+        #TODO: Event here?
 
     def _start_entity_process(self, entity) :
-        subprocess.Popen(('python entity.py %s' % entity.pickle()).split(' '))
+        p = subprocess.Popen(('python entity.py %s' % entity.pickle()).split(' '))
+        self._running_pids.add(p)
 
     def _on_sim_start(self, event) :
         local_entities = event.get_mapping()[self._phys_id]
@@ -34,4 +39,10 @@ class StartupDaemon :
 if __name__ == '__main__' :
     d = StartupDaemon(sys.argv[1])
     print 'Starting as %s' % (sys.argv[1],)
+
+    def quit(signal, frame) :
+        d.terminate_all()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, quit)
+
     d.start()
