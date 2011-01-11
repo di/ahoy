b32c3a938f281f1a5c4c4d479d4cbb1fba09a7fa
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2011-01-11 14:25:23 +0000

    Delete line fix

--- src/proto2/stage/testing/start.py
+++ src/proto2/stage/testing/start.py
@@ -12,11 +12,24 @@ from stage.tcpforward import TcpForward
 world = World()
 wlan = Network('wlan0')
 world.add_network(wlan)
+
+n1 = Node(0)
+n1.add_interface('wlan0', Interface(n1, wlan))
+n1.add_agent(CommsAgent(n1, 'wlan0', 1, False))
+n1.set_position(39.9534, -75.1912, 0.02)
+world.add_entity(n1)
+n2 = Node(1)
+n2.add_interface('wlan0', Interface(n2, wlan))
+n2.add_agent(CommsAgent(n2, 'wlan0', 0, True))
+n2.set_position(39.9534, -75.1912, 0.02)
+world.add_entity(n2)
+'''
 for i in range(0, 4) :
     n = Node(i)
     n.add_interface('wlan0', Interface(n, wlan))
     n.add_agent(CommsAgent(n, 'wlan0', (i+1) % 4))
     world.add_entity(n)
+'''
 
 if __name__ == '__main__' :
     def quit(signal, frame) :
