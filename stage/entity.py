5e899538b19af37ff8f1caa3352b568c99afa2b0
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2011-01-13 14:57:46 +0000

    SHE WORKS

--- stage/entity.py
+++ stage/entity.py
@@ -9,7 +9,7 @@ from stage.events.move import EntityMoveEvent
 from stage.util.geo import *
 
 class Entity :
-    TIC_INTERVAL = .5 # seconds
+    MAX_DISTANCE = 0.01 # km
     def __init__(self, uid) :
         self._uid = uid
         self._lat = 0
@@ -35,7 +35,7 @@ class Entity :
         Thread(target=self._move_tic, args=(lat, lon, agl, forward_vel, vert_vel)).start()
         
     def _move_tic(self, lat, lon, agl, forward_vel, vert_vel) :
-        last_tic = time.time() - Entity.TIC_INTERVAL
+        last_tic = time.time() - Entity.MAX_DISTANCE / forward_vel
         while lat != self._lat or lon != self._long or self._agl != agl :
             lon1 = math.radians(self._long)
             lon2 = math.radians(lon)
@@ -44,8 +44,7 @@ class Entity :
 
             y = math.sin(lon2 - lon1) * math.cos(lat2)
             x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(lon2 - lon1)
-            bearing = math.degrees(math.atan2(y, x))
-            #bearing = (bearing + 360) % 360
+            bearing = math.atan2(y, x)
 
             dt = time.time() - last_tic
 
@@ -54,12 +53,14 @@ class Entity :
             new_lat = math.degrees(math.asin(math.sin(lat1)*math.cos(d/R) + math.cos(lat1)*math.sin(d/R)*math.cos(bearing)))
             new_lon = math.degrees(lon1 + math.atan2(math.sin(bearing)*math.sin(d/R)*math.cos(lat1), math.cos(d/R)-math.sin(lat1)*math.sin(new_lat)))
 
-            print new_lat, new_lon
-           
+            if haver_distance(self._lat, self._long, lat, lon) < d :
+                self.set_position(lat, lon, agl)
+                break
+            print math.degrees(bearing), new_lat, new_lon
             self.set_position(new_lat, new_lon, agl)
 
             last_tic = time.time()
-            time.sleep(Entity.TIC_INTERVAL)
+            time.sleep(Entity.MAX_DISTANCE / forward_vel)
 
     def get_position(self) :
         return self._lat, self._long, self._agl
