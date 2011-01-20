38ddfc362c9619673c3070967a829a67692fd861
Frank Clark <francis.j.clark@gmail.com>
2011-01-20 20:07:08 +0000

    condition base class

---
+++ condition.py
@@ -0,0 +1,9 @@
+class Condition:
+    #name: what the condition is called
+    #state: state that satisfies the condition 
+    def __init__(self, name, state):
+        self.name_ = name
+        self.state_ = state
+
+    def isMet(self, state):
+        return
