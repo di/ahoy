4759a50f4a3ba0744e104188b1f7818301343f9d
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2011-01-20 20:18:49 +0000

    3 helis for demo

--- src/proto2/ahoy/testing/demo.py
+++ src/proto2/ahoy/testing/demo.py
@@ -10,18 +10,14 @@ from ahoy.agents.rectanglesurveil import RectangleSurveilAgent
 
 world = World()
 
-base = Node(0)
-base.set_position(39.9490, -75.1391, 0)
-base.add_agent(RectangleSurveilAgent(base, (39.9495, -75.1390), (39.9441, -75.1321), feet(74)))
-world.add_entity(base)
-
-'''
 heli_locs = [(39.9490, -75.1374), (39.9407, -75.1351), (39.9581, -75.1310)]
 for i, loc in enumerate(heli_locs) :
-    heli = Node(i + 1)
+    heli = Node(i)
     heli.set_position(loc[0], loc[1], feet(i * 100))
-    heli.add_agent(RectangleSurveil())
-'''
+    se = loc
+    nw = (loc[0] + .004, loc[1] - .003)
+    heli.add_agent(RectangleSurveilAgent(heli, nw, se, feet(75)))
+    world.add_entity(heli)
 
 if __name__ == '__main__' :
     sim = Simulation(world, LogLossComms(), TcpForward(int(sys.argv[1])))
