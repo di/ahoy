8afbd5d60a40a069882466deab1887aa03e2423d
Dustin Ingram <dustin.ingram@gmail.com>
2011-01-21 13:36:41 +0000

    Radar, etc supported

--- src/poc1/draw.py
+++ src/poc1/draw.py
@@ -17,6 +17,15 @@ image_surface = pygame.image.load("image.png")
 class ProofOfConcept :
     def __init__(self, ip, port) :
         self._nodelist = {}
+        self._nodecolor ={'Node':(100,100,255),'RadarSensor2':(255,100,100)}
+        self._radarlist = {}
+        self._current_radar_bearing = None
+        self._current_radar_loc = None
+
+        self.tl_lat, self.tl_lon = (39.960609,-75.17086)
+        self.br_lat, self.br_lon = (39.920993,-75.102625)
+        self.d_lat = self.br_lat-self.tl_lat
+        self.d_lon = self.br_lon-self.tl_lon
 
         dx, dy, ux, uy = 0,0,0,0
         gotFirst = False
@@ -29,6 +38,16 @@ class ProofOfConcept :
         self._event_api.subscribe(EntityMoveEvent, self._on_move)
         self._event_api.subscribe(RadarEvent, self._on_radar)
 
+    def _get_pix(self, lat, lon) :
+        x = int(((lon-self.tl_lon)/self.d_lon)*800)
+        y = int(((lat-self.tl_lat)/self.d_lat)*600)
+        return (x,y)
+
+    def _get_ll(self, x, y) :
+        lon = ((x/800.0)*self.d_lon)+self.tl_lon
+        lat = ((y/600.0)*self.d_lat)+self.tl_lat
+        return (lat,lon)
+
     def _on_link(self, event) :
         pass
 
@@ -36,33 +55,65 @@ class ProofOfConcept :
         uid = event.get_uid()
         lon = event.get_long()
         lat = event.get_lat()
-        # TODO Change this to do it the dumb way
-        x, y, z = sph_to_lin(lat, lon, 0)
-        dx, dy, dz = (1049.5729025143785, -3953.302616399923, 4884.6121796385296)
-        y = int((y - dy)/(5.808/800)) + 461
-        z = int((z - dz)/(4.356/600)) + 282
-        self._nodelist[uid] = (y, z)
+        type = event.get_type()
+        
+        self._nodelist[uid] = (self._get_pix(lat, lon),type)
 
     def _on_radar(self, event) :
-        pass
+        uid = event.get_sensor_id()
+        t_loc = event.get_target_location()
+        bear = round(math.degrees(event.get_bearing()))
+        (lat,lon,agl) = event.get_radar_loc()
+        
+        self._current_radar_bearing = bear
+        self._current_radar_loc = self._get_pix(lat, lon)
 
-    def send_bound(self, dy, dz, uy, uz) :
-        delta_x, delta_y, delta_z = (1049.5729025143785, -3953.302616399923, 4884.6121796385296)
-        dy = ((dy - 461) * (5.808/800)) + delta_y
-        dz = ((dz - 282) * (4.356/600)) + delta_z
-        uy = ((uy - 461) * (5.808/800)) + delta_y
-        uz = ((uz - 282) * (4.356/600)) + delta_z
+        self._radarlist[bear] = t_loc
+        #if t_loc is not None :
+        #    (t_lat,t_lon) = t_loc 
+            #print int(math.degrees(bear))
+            #print self._get_pix(t_lat, t_lon)
 
-        print lin_to_sph(0, dy, dz), lin_to_sph(0, uy, uz, )
+        self._nodelist[uid] = (self._get_pix(lat, lon),'RadarSensor2')
+
+    def send_bound(self, dx, dy, ux, uy) :
+        #print dx, dy, ux, uy
+        p1 = self._get_ll(dx, dy)
+        p2 = self._get_ll(ux, uy)
+
+        self._event_api.publish(RectangleSurveilMove(0, p1, p2))
+        print p1, p2
 
     def draw_nodes(self) :
         for uid in self._nodelist.keys() :
-            y, z = self._nodelist[uid]
-            self.draw_node((100,255,100), (y,z))
+            (x, y), type = self._nodelist[uid]
+            #print type
+            #self.draw_node((100,255,100), (x,y))
+            self.draw_node((x,y),type)
 
-    def draw_node(self, color, position) :
-        r, g, b = color
+    def draw_radar(self) :
+        if self._current_radar_loc is not None :
+            #print self._current_radar_bearing
+            x = min(max(0, int(self._current_radar_loc[0] + 800*math.cos(math.radians(self._current_radar_bearing)))),800)
+            y = min(max(0, int(self._current_radar_loc[1] + 600*math.sin(math.radians(self._current_radar_bearing)))),600)
+            pygame.draw.line(surface, (0,255,0), self._current_radar_loc, (x,y), 1) 
+        for bear in self._radarlist.keys() :
+            t_loc = self._radarlist[bear]
+            if t_loc is None :
+                del self._radarlist[bear]
+            else :
+                self.draw_blip(self._get_pix(*t_loc))
+
+    def draw_blip(self, position) :
         x, y = position
+        pygame.draw.circle(surface, (100,255,100), (x,y), 2, 0)
+
+    def draw_node(self, position, type) :
+        x, y = position
+        if self._nodecolor.has_key(type) :
+            r,g,b = self._nodecolor[type]
+        else :
+            r,g,b = (100,100,100)
 
         pygame.draw.circle(surface, (255,255,255), (x,y), 10, 0)
         pygame.draw.circle(surface, (r,g,b), (x,y), 6, 0)
@@ -78,10 +129,13 @@ def main() :
         pygame.display.flip()
         surface.blit(image_surface,(0,0))
         poc.draw_nodes()
+        poc.draw_radar()
 
     poc = ProofOfConcept(sys.argv[1], int(sys.argv[2]))
     signal.signal(signal.SIGINT, quit)
     redraw()
+    dx, dy, ux, uy = 0,0,0,0
+    gotFirst = False
 
     while True:
         for event in pygame.event.get():
@@ -94,9 +148,10 @@ def main() :
                 dx, dy = pygame.mouse.get_pos()
                 gotFirst = True
             ux, uy = pygame.mouse.get_pos()
-            pygame.draw.rect(surface,(255,0,0),(dx,dy,ux-dx,uy-dy),1)
-            poc.send_bound(dx, dy, ux, uy)
         else:
+            if gotFirst :
+                poc.send_bound(dx, dy, ux, uy)
             gotFirst = False    
+        pygame.draw.rect(surface,(255,0,0),(dx,dy,ux-dx,uy-dy),1)
         redraw() 
 main()
