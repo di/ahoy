dcceef6e9a12c84aad793eb117527017a0c6f03f
Dustin Ingram <dustin.ingram@gmail.com>
2011-02-05 11:10:58 +0000

    Also changed this

--- src/proto2/ahoy/events/sensor.py
+++ src/proto2/ahoy/events/sensor.py
@@ -4,3 +4,6 @@ class SensorEvent(Event) :
     def __init__(self, owner_uid) :
         Event.__init__(self)
         self._owner_uid = owner_uid
+
+    def get_owner_uid(self) :
+        return self._owner_uid
