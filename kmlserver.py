4271efe93dedea81400659ab2f6182f02d9034ab
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2011-02-11 11:56:16 +0000

    Adding KML server for Google Earth

---
+++ kmlserver.py
@@ -0,0 +1,79 @@
+import socket
+import sys
+from threading import Thread
+from ahoy.events.move import EntityMoveEvent
+from ahoy.eventapi import EventAPI
+
+class KmlServer :
+    def __init__(self, port) :
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.bind(('', port))
+        self._sock.listen(1)
+        self._event_api = EventAPI()
+        self._event_api.start()
+        self._event_api.subscribe(EntityMoveEvent, self._on_move)
+        self._pos = {}
+        self._first = True
+
+    def _on_move(self, event) :
+        self._pos[event.get_uid()] = (event.get_lat(), event.get_long(), event.get_agl())
+
+    def _get_contents(self) :
+        s = ''
+        for uid, loc in self._pos.iteritems() :
+            lat, long, agl = loc
+            if self._first :
+                s += '''<Placemark>
+                <Model>
+                <altitudeMode>absolute</altitudeMode>
+                <Location id="%s">
+                <longitude>%s</longitude>
+                <latitude>%s</latitude>
+                <altitude>%s</altitude>
+                </Location>
+                <Link>
+                <href>file:///Users/arosenfeld/ahoy/trunk/src/proto2/ahoy/ss_united_states.dae</href>
+                </Link>
+                </Model>
+                </Placemark>\n''' % (uid, long, lat, agl)
+            else :
+                s += '''
+                <Update>
+                <Change>
+                <Location targetId="%s">
+                    <longitude>%s</longitude>
+                    <latitude>%s</latitude>
+                    <altitude>%s</altitude>
+                </Location>
+                </Change>
+                </Update>
+                \n''' % (uid, long, lat, agl)
+
+        if self._first :
+            self._first = False
+            s = '<Document>\n' + s + '</Document>\n'
+        else :
+            s = '<NetworkLinkControl>\n' + s + '</NetworkLinkControl>\n'
+        return s
+
+    def _handle(self, conn) :
+        data = 'HTTP/1.1 200 OK\n'
+        data += 'Content-Type: application/vnd.google-earth.kml+xml\n\n'
+        contents = self._get_contents()
+        if contents != None :
+            data += '<?xml version="1.0" encoding="UTF-8"?>\n'
+            data += '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
+            data += contents
+            data += '</kml>'
+            conn.send(data)
+            print data
+            print '\n'
+        conn.close()
+
+    def start(self) :
+        while True :
+            conn, addr = self._sock.accept()
+            Thread(target=self._handle, args=(conn,)).start()
+
+if __name__ == '__main__' :
+    KmlServer(int(sys.argv[1])).start()
