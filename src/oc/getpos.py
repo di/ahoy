93c8cc587b9a00268baf2b23bfe2d6a3d0d7ccd7
Frank Clark <francis.j.clark@gmail.com>
2011-04-26 22:02:59 +0000

    Updated getpos to be more accurate

--- src/oc/getpos.py
+++ src/oc/getpos.py
@@ -12,36 +12,38 @@ from ahoy.events.move import EntityMoveEvent
 from ahoy.sensors.radarsensor import RadarEvent
 
 pygame.init()
-surface = pygame.display.set_mode((1341,744))
-image_surface = pygame.image.load("river.png")
-lat_comp = 0.004086 
-lon_comp = 0.001935
+pygame.display.set_caption("Path Finder 2000")
+surface = pygame.display.set_mode((1280,800))
+image_surface = pygame.image.load("map_big.png")
 # 461, 282
+center = (-1770,-2000)
+window_center = (-1770,-2000)
+
 class LatLonMapper :
     #def __init__(self, ip, port) :
     def __init__(self):
-        self.tl_lat, self.tl_lon = (40.0037,-75.456)
-        self.br_lat, self.br_lon = (39.815,-74.994)
+
+        self.tl_lat, self.tl_lon = (40.0140,-75.3321)
+        self.br_lat, self.br_lon = (39.7590,-75.0000)
         self.d_lat = self.br_lat-self.tl_lat
         self.d_lon = self.br_lon-self.tl_lon
-
         dx, dy, ux, uy = 0,0,0,0
         gotFirst = False
 
 
-    def get_pix(self, lat, lon) :
+    def _get_pix(self, lat, lon) :
+        global center
+        cx, cy = center
         try :
-            x = int(((lon-self.tl_lon)/self.d_lon)*1341)
-            y = int(((lat-self.tl_lat)/self.d_lat)*744)
+            x = int(((lon-self.tl_lon)/self.d_lon)*4800)+cx
+            y = int(((lat-self.tl_lat)/self.d_lat)*4800)+cy
             return (x,y)
         except :
             return (0,0)
 
-    def get_ll(self, x, y) :
-        lon = ((x/1341.0)*self.d_lon)+self.tl_lon
-        lat = ((y/744.0)*self.d_lat)+self.tl_lat
-        lon = lon + lon_comp
-        lat = lat - lat_comp
+    def _get_ll(self, x, y) :
+        lon = ((x/4800.0)*self.d_lon)+self.tl_lon
+        lat = ((y/4800.0)*self.d_lat)+self.tl_lat
         return (lat,lon)
 
 def main() :
@@ -71,7 +73,7 @@ def main() :
                 quit(None, None)
                 #pygame.quit(); sys.exit()
 
-        if pygame.mouse.get_pressed()[0] :
+        if pygame.mouse.get_pressed()[0] and (pygame.key.get_mods() & KMOD_SHIFT):
             ux, uy = pygame.mouse.get_pos()
             lat, lon = poc.get_ll(ux,uy) 
             if(not(lat == lastlat) and not(lon == lastlon)):
@@ -85,6 +87,7 @@ def main() :
                 f.write("@@@\n")
                 print "Ending this path"
                 ended = True
+
         redraw() 
         #time.sleep(1)
 main()
