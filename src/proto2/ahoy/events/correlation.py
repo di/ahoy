2ade52bae62a1e29ac34421e33a8d172f1873d34
Dustin Ingram <dustin.ingram@gmail.com>
2011-04-26 21:02:47 +0000

    Fixes

--- src/proto2/ahoy/events/correlation.py
+++ src/proto2/ahoy/events/correlation.py
@@ -2,10 +2,10 @@ from ahoy.event import Event
 
 # An event with a pair of lat/lons which are correlated
 class CorrelationEvent(Event) :
-    def __init__(self, point1, point2) :
+    def __init__(self, point1_lat, point1_lon, point2_lat, point2_lon) :
         Event.__init__(self)
-        self._point1 = point1
-        self._point2 = point2
+        self._point1 = (point1_lat, point1_lon)
+        self._point2 = (point2_lat, point2_lon)
 
     def get_locations(self) :
         return (self._point1, self._point2) 
