807266548cd2c682922a0998f90a9bbe144f0f4b
Aaron Rosenfeld <aaron@aaron-rosenfeld.com>
2011-05-09 23:40:35 +0000

    Spill event for threat agent

--- src/proto2/ahoy/agents/threat.py
+++ src/proto2/ahoy/agents/threat.py
@@ -6,6 +6,7 @@ from ahoy.agent import Agent
 from ahoy.message import Message
 from ahoy.util.aisdatagen import AISDataGen
 from ahoy.events.move import EntityMoveEvent
+from ahoy.events.chemical import ChemicalSpillEvent
 from ahoy.util.geo import *
 
 class ThreatShip(Agent) :
@@ -30,9 +31,9 @@ class ThreatShip(Agent) :
         if(self._follow != None):
             self.get_owner_node().get_event_api().subscribe(EntityMoveEvent, self._on_tanker_move)
 
-        print "Threat at " + lat + "," + lon
+        print 'Threat at ' + lat + ',' + lon
         for l in self._locs[1:]:
-            print "Threat moving to " + l
+            print 'Threat moving to ' + l
             self._move(l)            
 
 
@@ -46,18 +47,21 @@ class ThreatShip(Agent) :
 
     def _on_tanker_move(self, event):
         if event.get_uid() == self._follow:
-            print "Got the follow event"
+            print 'Got the follow event'
             lat = event.get_lat()
             lon = event.get_long()
             vel = event.get_forward_vel()
             mylat,mylon,myagl = self.get_owner_node().get_position()
             d = lin_distance(lat,lon,0.0,mylat,mylon,0.0)
-            print str(d) + " kilos away"
-            if(d <= 0.2 and vel != 0):
-                print "Slowing down..."
+            print str(d) + ' kilos away'
+            if d == 0 :
+                print 'Intercepted ship'
+                self.get_owner_node().get_event_api().publish(ChemicalSpillEvent(self.get_owner_node().get_position(), .0002))
+            elif d <= 0.2 and vel != 0 :
+                print 'Slowing down...'
                 self._forward_vel = vel
-            elif(d > 0.2):
-                print "Speeding up..."
+            elif d > 0.2 :
+                print 'Speeding up...'
                 self._forward_vel = vel + 0.08         
             
             self.get_owner_node().set_forward_velocity(self._forward_vel)
