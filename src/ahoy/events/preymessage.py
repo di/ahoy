5fa48628e98f0e09a5a05364a863335b8e6a7485
Dustin Ingram <dustin.ingram@gmail.com>
2011-05-11 15:06:19 +0000

    Prey deathmessage

---
+++ src/ahoy/events/preymessage.py
@@ -0,0 +1,13 @@
+from ahoy.event import Event
+
+class PredatorMessage(Event) :
+    def __init__(self, src_agent_uid, alive) :
+        Event.__init__(self)
+        self._src_agent_uid = src_agent_uid
+        self._alive = alive
+
+    def get_src_agent_uid(self) :
+        return self._src_agent_uid
+
+    def is_alive(self) :
+        return self._alive
